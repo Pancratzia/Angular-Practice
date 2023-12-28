fd79c8a503ce5c906dfe0fe1a9ed7b8636eda3ce
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-28 17:01:24 -0400

    Modificando el componente App

--- ubaldo-acosta-primera-app/src/app/app.component.ts
+++ ubaldo-acosta-primera-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ubaldo-acosta-primera-app';
+  title = 'Mi Primera App de Angular';
+  autor = 'Laura Ortega';
 }
