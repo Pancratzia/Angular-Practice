73cdd7edcdbe2663fc394a6c580fc0387bd20a67
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-30 14:56:27 -0400

    Event binding pt. 2

--- ubaldo-acosta-primera-app/src/app/personas/personas.component.ts
+++ ubaldo-acosta-primera-app/src/app/personas/personas.component.ts
@@ -18,7 +18,12 @@ import { Component } from '@angular/core';
 export class PersonasComponent {
   deshabilitar: boolean = false;
   mensaje: string = 'No se ha agregado ninguna persona';
+  titulo: string = '';
   agregarPersona(){
     this.mensaje = 'Persona Agregada'
+  }
+
+  modificarTitulo(event: Event){
+    this.titulo = (<HTMLInputElement>event.target).value;
   }
 }
