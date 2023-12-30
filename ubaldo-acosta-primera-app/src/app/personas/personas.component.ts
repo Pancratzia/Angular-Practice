34d292da818f7bd15753994fdd508a78cb125176
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-29 23:36:54 -0400

    Using templatesUrl by default and adding bootstrap styles

--- ubaldo-acosta-primera-app/src/app/personas/personas.component.ts
+++ ubaldo-acosta-primera-app/src/app/personas/personas.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  //templateUrl: './personas.component.html',
-  styleUrls: ['./personas.component.css'],
-  template: `<h2>Listado de Personas</h2>
+  templateUrl: './personas.component.html',
+  //styleUrls: ['./personas.component.css'],
+  /*template: `<h2>Listado de Personas</h2>
     <ol>
       <app-persona></app-persona>
       <app-persona></app-persona>
     </ol> `,
+  styles: [`
+  ol{
+    background-color: red;
+  }
+  `]*/
 })
 export class PersonasComponent {}
