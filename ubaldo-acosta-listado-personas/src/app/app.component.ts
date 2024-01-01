2f12ffa86f81234305c39d4ae33270c8ef9937f5
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-01 11:04:40 -0400

    Creando un modelo para la clase persona

--- ubaldo-acosta-listado-personas/src/app/app.component.ts
+++ ubaldo-acosta-listado-personas/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ubaldo-acosta-listado-personas';
+  title = 'Listado de Personas';
 }
