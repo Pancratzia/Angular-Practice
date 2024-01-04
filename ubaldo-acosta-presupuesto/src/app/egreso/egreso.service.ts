f40e5e718c4184fa2ce1b106ecb55e80adc685b0
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-04 12:20:22 -0400

    Finalizando la parte de los egresos para que se muestren de forma dinamica

--- ubaldo-acosta-presupuesto/src/app/egreso/egreso.service.ts
+++ ubaldo-acosta-presupuesto/src/app/egreso/egreso.service.ts
@@ -6,4 +6,9 @@ export class EgresoServicio{
         new Egreso("Alquiler", 1500),
         new Egreso("Comida", 700),
     ];
+
+    eliminar(egreso:Egreso){
+        const indice: number = this.egresos.indexOf(egreso);
+        this.egresos.splice(indice,1);
+    }
 }
