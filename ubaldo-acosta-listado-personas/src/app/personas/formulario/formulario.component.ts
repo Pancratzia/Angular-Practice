58bec00db3d017b198c4d07931d801ef547a2aa6
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-09 18:28:54 -0400

    Usando query params

--- ubaldo-acosta-listado-personas/src/app/personas/formulario/formulario.component.ts
+++ ubaldo-acosta-listado-personas/src/app/personas/formulario/formulario.component.ts
@@ -15,6 +15,7 @@ export class FormularioComponent implements OnInit {
   nombreInput: string= '';
   apellidoInput: string='';
   index:number;
+  modoEdicion:number;
 
   constructor(private personaService: PersonasService, private router:Router,
     private route: ActivatedRoute){
@@ -24,8 +25,9 @@ export class FormularioComponent implements OnInit {
   }
   ngOnInit(): void {
     this.index = this.route.snapshot.params['id'];
+    this.modoEdicion = +this.route.snapshot.queryParams['modoEdicion'];
 
-    if(this.index){
+    if(this.modoEdicion !== null && this.modoEdicion === 1){
       let persona : Persona = this.personaService.encontrarPersona(this.index);
       this.nombreInput = persona.nombre;
       this.apellidoInput = persona.apellido;
@@ -34,7 +36,7 @@ export class FormularioComponent implements OnInit {
 
   onGuardarPersona() : void {
     let newPersona = new Persona(this.nombreInput.toUpperCase(), this.apellidoInput.toUpperCase());
-    if(this.index){
+    if(this.modoEdicion !== null && this.modoEdicion === 1){
       this.personaService.modificarPersona(this.index, newPersona);
     }else{
       this.personaService.agregarPersona(newPersona);
