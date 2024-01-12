afcd5d4cd9c57e401663a0f3ac0027b72286b2d1
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-12 18:34:55 -0400

    Validando en su totalidad el formulario de cliente

--- control-clientes/src/app/componentes/clientes/clientes.component.ts
+++ control-clientes/src/app/componentes/clientes/clientes.component.ts
@@ -7,57 +7,61 @@ import { ClienteServicio } from 'src/app/servicios/cliente.service';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  styleUrls: ['./clientes.component.css'],
 })
-
 export class ClientesComponent implements OnInit {
-
   clientes: Cliente[];
-  cliente : Cliente = {
+  cliente: Cliente = {
     nombre: '',
     apellido: '',
     email: '',
-    saldo: 0
-  }
+    saldo: 0,
+  };
 
-  constructor(private clienteServicio : ClienteServicio, private alertMessages : AlertMessagesService) { }
+  constructor(
+    private clienteServicio: ClienteServicio,
+    private alertMessages: AlertMessagesService
+  ) {}
 
   ngOnInit(): void {
-    this.clienteServicio.getClientes().subscribe(
-      clientes => this.clientes = clientes
-    );
+    this.clienteServicio
+      .getClientes()
+      .subscribe((clientes) => (this.clientes = clientes));
   }
 
-  getTotalClientes(): number{
-    if(this.clientes){
+  getTotalClientes(): number {
+    if (this.clientes) {
       return this.clientes.length;
     }
     return 0;
   }
 
-  getSaldoTotal(): number{
+  getSaldoTotal(): number {
     let saldoTotal: number = 0;
-    if(this.clientes){
-      this.clientes.forEach(cliente => {
+    if (this.clientes) {
+      this.clientes.forEach((cliente) => {
         saldoTotal += cliente.saldo || 0;
       });
     }
     return saldoTotal;
   }
 
-  agregar(form : NgForm): void{
-    
-    let [value, valid] = [form.value, form.valid];
-    if(!valid){
+  agregar(form: NgForm): void {
+    console.log(form.value); // Verifica los valores del formulario
+    console.log(form.controls['nombre'].errors);
 
-      this.alertMessages.show('Por favor, rellena el formulario correctamente', {
-        cssClass: 'alert-danger text-center d-flex justify-content-between m-1',
-        timeOut: 3000
-      })
-    }else{
+    let [value, valid] = [form.value, form.valid];
+    if (!valid) {
+      this.alertMessages.show(
+        'Por favor, rellena el formulario correctamente',
+        {
+          cssClass:
+            'alert-danger text-center d-flex justify-content-between m-1',
+          timeOut: 3000,
+        }
+      );
+    } else {
       //Agregar el nuevo cliente
     }
-
   }
-
 }
