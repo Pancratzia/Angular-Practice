f3bc7350f7ac3d1db14ef155591ed944ad26c72c
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-13 11:47:34 -0400

    Empezando el componente de editar cliente

--- control-clientes/src/app/componentes/editar-cliente/editar-cliente.component.ts
+++ control-clientes/src/app/componentes/editar-cliente/editar-cliente.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertMessagesService } from 'jjwins-angular-alert-messages';
+import { Cliente } from 'src/app/modelos/cliente.model';
+import { ClienteServicio } from 'src/app/servicios/cliente.service';
 
 @Component({
   selector: 'app-editar-cliente',
   templateUrl: './editar-cliente.component.html',
   styleUrls: ['./editar-cliente.component.css']
 })
-export class EditarClienteComponent {
+export class EditarClienteComponent implements OnInit {
+
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0,
+  };
+
+  id: string;
+
+  constructor(
+    private clienteServicio: ClienteServicio,
+    private alertMessages: AlertMessagesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+  }
 
 }
