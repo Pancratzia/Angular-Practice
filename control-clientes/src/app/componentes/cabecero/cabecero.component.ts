66d0628e970733420bbb35a10cc6f4b0470a04c0
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-14 14:10:46 -0400

    Obteniendo en el cabecero si el usuario esta autenticado o no

--- control-clientes/src/app/componentes/cabecero/cabecero.component.ts
+++ control-clientes/src/app/componentes/cabecero/cabecero.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-cabecero',
   templateUrl: './cabecero.component.html',
   styleUrls: ['./cabecero.component.css']
 })
-export class CabeceroComponent {
+export class CabeceroComponent implements OnInit {
+
+  isLoggedIn: boolean;
+  loggedInUser: string | null;
+
+  constructor(private loginService : LoginService, privateRouter : Router) { }
+
+  ngOnInit(): void {
+    this.loginService.getAuth().subscribe( auth => {
+      if(auth){
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+      } else {
+        this.isLoggedIn = false;
+      }
+    })
+  }
 
 }
