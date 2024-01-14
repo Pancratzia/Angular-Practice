b8e4c6dae4f3ecc0d1cf16228895811241423f53
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-14 15:10:05 -0400

    Creando el formulario de registro y el servicio de registrarse

--- control-clientes/src/app/componentes/registro/registro.component.ts
+++ control-clientes/src/app/componentes/registro/registro.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertMessagesService } from 'jjwins-angular-alert-messages';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
+
+  email: string;
+  password: string;
+
+  constructor(private router : Router, private alerMessages : AlertMessagesService, private loginService : LoginService) { }
+
+  ngOnInit(): void {
+    this.loginService.getAuth().subscribe( auth => {
+      if(auth){
+        this.router.navigate(['/']);
+      }
+    })
+  }
+
+  registro(){
+    
+  }
 
 }
