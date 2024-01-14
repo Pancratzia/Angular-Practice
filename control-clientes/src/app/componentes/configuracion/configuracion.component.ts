a11e493b5f9356b9343af0dda664ba4b4406767f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-14 15:18:48 -0400

    Empezando con la vista del componente de configuracion

--- control-clientes/src/app/componentes/configuracion/configuracion.component.ts
+++ control-clientes/src/app/componentes/configuracion/configuracion.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-configuracion',
   templateUrl: './configuracion.component.html',
   styleUrls: ['./configuracion.component.css']
 })
-export class ConfiguracionComponent {
+export class ConfiguracionComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  guardar(){
+
+  }
 
 }
