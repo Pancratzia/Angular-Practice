da8d9a14a261c3477e58f0cfbceba7a90d2fd9c3
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-14 16:08:19 -0400

    Aplicacion lista para el deploy en firebase

--- control-clientes/src/app/app.module.ts
+++ control-clientes/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule, SETTINGS } from '@angular/fire/compat/firestore';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
